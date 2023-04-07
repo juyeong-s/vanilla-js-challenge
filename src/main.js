273f998fcd186ba9616bf644f3fea176dd12ab2c
juyeong-chungbuk <happyu9986@gmail.com>
2023-04-08 01:29:06 +0900

    delete: 필요없는 파일 삭제

--- src/main.js
+++
@@ -1,3 +0,0 @@
-import App from "@/App.js";
-const app = new App();
-document.body.appendChild(app.$wrapper);
--- src/utils/getPath.js
+++
@@ -1,5 +0,0 @@
-function getPath() {
-  return location.pathname;
-}
-
-export default getPath;
